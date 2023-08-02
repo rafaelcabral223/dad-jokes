3e7e20b8dd0abfd035da5dd2a3a5d32af93974a8
Rafael <rafaelcpribeiro@gmail.com>
2023-08-02 20:07:25 -0300

    update final code

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log('oi')
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+//USING .then()
+function generateJoke() {
+
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    fetch('https://icanhazdadjoke.com', config).then(response => response.json()).then(data => {
+        jokeEl.innerHTML = data.joke
+    })
+}
+
+
+//USING ASYNC/AWAIT
+// 
+//async function generateJoke() {
+//    const config = {
+//        headers: {
+//            Accept : "application/json"
+//        },
+//    }
+//
+//    const response = await fetch('ttps://icanhazdadjoke.com', config)
+//
+//    const data = await response.json()
+//    
+//    jokeEl.innerHTML = data.joke
+//}
